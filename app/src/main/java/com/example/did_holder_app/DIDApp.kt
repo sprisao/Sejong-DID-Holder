16e27ce6819a097a7822ccfddfbf2dc5d939feb0
sprisao <brucechoe@sejonggroup.kr>
2023-01-05 15:29:43 +0900

    DidApp 추가

---
+++ app/src/main/java/com/example/did_holder_app/DIDApp.kt
@@ -0,0 +1,14 @@
+package com.example.did_holder_app
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+
+enum class DIDScreen() {
+    DID,
+    VC,
+    QR
+}
+@Composable
+fun DIDApp(modifier: Modifier = Modifier) {
+}
