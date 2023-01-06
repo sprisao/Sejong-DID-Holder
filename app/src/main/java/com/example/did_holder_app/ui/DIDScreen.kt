01cbbe78c1699606cddfb63c9e50447b053413e3
sprisao <brucechoe@sejonggroup.kr>
2023-01-06 09:15:04 +0900

    RSA Algorithm을 통해 비대칭 키 생성

--- app/src/main/java/com/example/did_holder_app/ui/DIDScreen.kt
+++ app/src/main/java/com/example/did_holder_app/ui/DIDScreen.kt
@@ -8,9 +8,23 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import timber.log.Timber
+import java.security.KeyPair
+import java.security.KeyPairGenerator
+import java.security.PrivateKey
+import java.security.PublicKey
+import java.security.Signature
 
 @Composable
 fun DIDScreen() {
+    val didDoc = DidDocument()
+
+    val publicKey = didDoc.publicKey
+    val privateKey = didDoc.privateKey
+
+    Timber.d("Public Key: $publicKey")
+    Timber.d("Private Key: $privateKey")
+
     Column(
         modifier = Modifier
             .fillMaxSize()
@@ -34,3 +48,35 @@ fun GenerateDIDButton(modifier: Modifier) {
     }
 }
 
+
+class DidDocument {
+    /* 1. Generate Asymmetric Key */
+    val keyPair: KeyPair = KeyPairGenerator.getInstance("RSA").apply {
+        initialize(2048)
+    }.generateKeyPair()
+
+    /*public key*/
+
+    val publicKey: PublicKey = keyPair.public
+
+    val privateKey: PrivateKey = keyPair.private
+
+    fun signMessage(privateKey: PrivateKey, message: String): String {
+        val signature = Signature.getInstance("SHA256withRSA")
+        signature.initSign(privateKey)
+        signature.update(message.toByteArray())
+        return signature.sign().toString()
+    }
+
+    fun verifySignature(
+        publicKey: PublicKey,
+        message: String,
+        signature: ByteArray
+    ): Boolean {
+        val sig = Signature.getInstance("SHA256withRSA")
+        sig.initVerify(publicKey)
+        sig.update(message.toByteArray())
+        return sig.verify(signature)
+    }
+
+}
