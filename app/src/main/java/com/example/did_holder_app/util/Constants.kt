255c27b8b46a57dd23deed8b099cde0d9f9eb058
sprisao <brucechoe@sejonggroup.kr>
2023-01-09 15:17:08 +0900

    [Room] DID, Public_KEY Room에 저장하기 : Constants에 DATASTORE_NAME 추가

--- app/src/main/java/com/example/did_holder_app/util/Constants.kt
+++ app/src/main/java/com/example/did_holder_app/util/Constants.kt
@@ -4,4 +4,15 @@ object Constants {
     const val DID = "DID"
     const val VC = "VC"
     const val QR = "QR"
+
+    const val DID_KEYSTORE_ALIAS = "did_key"
+    const val VC_KEYSTORE_ALIAS = "vc_key"
+    const val QR_KEYSTORE_ALIAS = "qr_key"
+
+    const val DID_KEYSTORE_TYPE = "AndroidKeyStore"
+    const val VC_KEYSTORE_TYPE = "AndroidKeyStore"
+    const val QR_KEYSTORE_TYPE = "AndroidKeyStore"
+
+    const val DATASTORE_NAME = "DID_DATASTORE"
+
 }
