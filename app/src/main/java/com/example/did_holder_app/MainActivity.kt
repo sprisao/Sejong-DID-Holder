cd83e0b11cedd297f8f82c1b2fe83556e74a3267
sprisao <brucechoe@sejonggroup.kr>
2023-01-09 15:18:09 +0900

    [Room] DID, Public_KEY Room에 저장하기 : ViewModelProviderFactory 적용

--- app/src/main/java/com/example/did_holder_app/MainActivity.kt
+++ app/src/main/java/com/example/did_holder_app/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.example.did_holder_app
 
-import android.annotation.SuppressLint
 import android.content.Context
 import android.os.Build
 import android.os.Bundle
@@ -9,30 +8,19 @@ import androidx.activity.compose.setContent
 import androidx.annotation.RequiresApi
 import androidx.compose.foundation.layout.*
 import androidx.compose.material3.*
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.datastore.preferences.preferencesDataStore
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.lifecycleScope
-import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.did_holder_app.data.DIDRepositoryImpl
 import com.example.did_holder_app.ui.theme.DID_Holder_AppTheme
 import com.example.did_holder_app.ui.viewmodel.DIDViewModel
 import com.example.did_holder_app.ui.viewmodel.DIDViewModelProviderFactory
-import com.example.did_holder_app.util.AndroidKeyStoreUtil.generateAndSaveKey
-import com.example.did_holder_app.util.AndroidKeyStoreUtil.loadAndDecryptKey
 import com.example.did_holder_app.util.Constants.DATASTORE_NAME
-import kotlinx.coroutines.launch
 import timber.log.Timber
 
 class MainActivity : ComponentActivity() {
 
     private val Context.dataStore by preferencesDataStore(DATASTORE_NAME)
-    lateinit var didViewModel: DIDViewModel
+    private lateinit var didViewModel: DIDViewModel
 
     @RequiresApi(Build.VERSION_CODES.O)
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,8 +29,8 @@ class MainActivity : ComponentActivity() {
         if (BuildConfig.DEBUG) {
             Timber.plant(Timber.DebugTree())
         }
-        val DIDRepository = DIDRepositoryImpl(dataStore)
-        val factory = DIDViewModelProviderFactory(DIDRepository, this)
+        val didRepository = DIDRepositoryImpl(dataStore)
+        val factory = DIDViewModelProviderFactory(didRepository, this)
 
         didViewModel = ViewModelProvider(this, factory)[DIDViewModel::class.java]
 
