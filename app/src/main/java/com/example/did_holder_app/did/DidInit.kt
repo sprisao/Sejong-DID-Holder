9fb6abdd3114f7ee13a67cd54953600770f600ae
sprisao <brucechoe@sejonggroup.kr>
2023-01-10 18:14:36 +0900

    [DIDInit] DID, Pubkey 생성 및 dataStore 저장 로직 제거 -> DidDocument로 통합

--- app/src/main/java/com/example/did_holder_app/did/DidInit.kt
+++ app/src/main/java/com/example/did_holder_app/did/DidInit.kt
@@ -1,26 +1,14 @@
 package com.example.did_holder_app.did
 
-import android.content.Context
 import android.util.Base64
 import com.example.did_holder_app.data.model.DidDocument
 import com.example.did_holder_app.util.AndroidKeyStoreUtil
 import com.example.did_holder_app.util.Constants.DID_METHODE
-import com.example.did_holder_app.util.DidDataStore
-import com.squareup.moshi.JsonAdapter
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
-import timber.log.Timber
 import java.security.*
 
-class DidInit(context: Context) {
+class DidInit {
 
-    private val didDataStore = DidDataStore(context)
-
-    val moshi = com.squareup.moshi.Moshi.Builder()
-        .add(KotlinJsonAdapterFactory())
-        .build()
-    val jsonAdapter: JsonAdapter<DidDocument> = moshi.adapter(DidDocument::class.java)
-
-    suspend fun generateDID(): String {
+    fun generateDidDocument(): DidDocument {
 
         /* Generate Asymmetric Keypair*/
         val keyPair: KeyPair = KeyPairGenerator.getInstance("RSA").apply {
@@ -29,9 +17,6 @@ class DidInit(context: Context) {
 
         val publicKey: PublicKey = keyPair.public
         val privateKey: PrivateKey = keyPair.private
-
-        /* save original public Key in datastore*/
-        didDataStore.savePublicKey(publicKey.toString())
 
         /* encrypt and save private Key in Keystore*/
         val encryptedPrivateKey: ByteArray =
@@ -44,26 +29,31 @@ class DidInit(context: Context) {
         val md = MessageDigest.getInstance("SHA-256")
         val encryptedPubKey = md.digest(message.toByteArray())
 
-        // DID Url 부분
-        val encodedPubKey = Base64.encodeToString(encryptedPubKey, Base64.NO_WRAP)
+        val hashedPubKey = Base64.encodeToString(encryptedPubKey, Base64.DEFAULT)
 
+        // DID Url 부분
+        val didIdentifier = Base64.encodeToString(encryptedPubKey, Base64.NO_WRAP)
 
         val didContext = "https://www.w3.org/ns/did/v1"
-        val didId = "did:$DID_METHODE:$encodedPubKey"
+
+        val didId = "did:$DID_METHODE:$didIdentifier"
+
         val didPublicKey = listOf(
             com.example.did_holder_app.data.model.PublicKey(
                 controller = didId,
                 id = "$didId#keys-1",
-                publicKeyBase58 = publicKey.toString(),
+                publicKeyBase64 = hashedPubKey.toString(),
                 type = "RSAVerificationKey2023"
             )
         )
+
         val didAuthentication = listOf(
             com.example.did_holder_app.data.model.Authentication(
                 type = "RSASignatureAuthentication2023",
                 publicKey = "$didId#keys-1"
             )
         )
+
         val didService = listOf(
             com.example.did_holder_app.data.model.Service(
                 id = "$didId;indx",
@@ -71,22 +61,15 @@ class DidInit(context: Context) {
                 serviceEndpoint = "https://example.com/indx"
             )
         )
-        val didDocument = DidDocument(
+
+        return DidDocument(
             context = didContext,
             id = didId,
             publicKey = didPublicKey,
             authentication = didAuthentication,
             service = didService
         )
-
-        val didDocumentJson = jsonAdapter.toJson(didDocument)
-
-        Timber.d("didDocumentJson: $didDocumentJson")
-
-        return "did:sjbr:${encodedPubKey}"
     }
-
-    /*DidDocument data class로 */
 
 
     // ------ DID Auth 부분 ------
