e798b3f41ecd9c76bf1f19db3e7656acfc3152a5
sprisao <brucechoe@sejonggroup.kr>
2023-01-10 18:17:27 +0900

    [DidDataStore] Pubkey, DID 관련 저장 및 get 제거

--- app/src/main/java/com/example/did_holder_app/util/DidDataStore.kt
+++ app/src/main/java/com/example/did_holder_app/util/DidDataStore.kt
@@ -4,49 +4,48 @@ import android.content.Context
 import androidx.datastore.preferences.core.edit
 import androidx.datastore.preferences.core.stringPreferencesKey
 import androidx.datastore.preferences.preferencesDataStore
+import com.example.did_holder_app.data.model.DidDocument
+import com.squareup.moshi.JsonAdapter
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.map
 import timber.log.Timber
 
 class DidDataStore(private val context: Context) {
+
+    val moshi: Moshi = Moshi.Builder()
+        .add(KotlinJsonAdapterFactory())
+        .build()
+    private val jsonAdapter: JsonAdapter<DidDocument> =
+        moshi.adapter(DidDocument::class.java)
+
     private object PreferencesKeys {
-        val DID = stringPreferencesKey("did")
-        val PUBLIC_KEY = stringPreferencesKey("public_key")
+        val DID_DOCUMENT = stringPreferencesKey("did_document")
     }
 
     companion object {
-        const val DATASTORE_NAME = "did_datastore"
         private val Context.dataStore by preferencesDataStore(Constants.DATASTORE_NAME)
     }
 
-    val getDid: Flow<String?> = context.dataStore.data.map { preferences ->
-        preferences[PreferencesKeys.DID] ?: ""
-    }
-
-    val getPublicKey: Flow<String?> = context.dataStore.data.map { preferences ->
-        preferences[PreferencesKeys.PUBLIC_KEY] ?: ""
-    }
-
-    suspend fun saveDid(did: String) {
-        context.dataStore.edit { preferences ->
-            preferences[PreferencesKeys.DID] = did
+    val getDidDocument: Flow<DidDocument?> = context.dataStore.data.map { preferences ->
+        preferences[PreferencesKeys.DID_DOCUMENT]?.let {
+            Timber.d("getDidDocument: $it")
+            jsonAdapter.fromJson(it)
         }
     }
 
-    suspend fun deleteDid() {
+    suspend fun saveDidDocument(didDocument: String) {
         context.dataStore.edit { preferences ->
-            preferences.remove(PreferencesKeys.DID)
+            preferences[PreferencesKeys.DID_DOCUMENT] = didDocument
         }
-        context.dataStore.edit { preferences ->
-            preferences.remove(PreferencesKeys.PUBLIC_KEY)
-        }
-        Timber.d("did deleted")
     }
 
-    suspend fun savePublicKey(publicKey: String) {
+    suspend fun deleteDidDocument() {
         context.dataStore.edit { preferences ->
-            preferences[PreferencesKeys.PUBLIC_KEY] = publicKey
+            preferences.remove(PreferencesKeys.DID_DOCUMENT)
         }
     }
+
 
 }
