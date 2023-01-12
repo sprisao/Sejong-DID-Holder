1168efdb4b1197dfb3e8d1aafea2073d8323046e
sprisao <brucechoe@sejonggroup.kr>
2023-01-12 13:50:29 +0900

    BarCodeAnalyser.kt 클래스 생성

---
+++ app/src/main/java/com/example/did_holder_app/util/BarCodeAnalyser.kt
@@ -0,0 +1,52 @@
+package com.example.did_holder_app.util
+
+import android.annotation.SuppressLint
+import androidx.camera.core.ImageAnalysis
+import androidx.camera.core.ImageProxy
+import com.google.mlkit.vision.barcode.BarcodeScannerOptions
+import com.google.mlkit.vision.barcode.BarcodeScanning
+import com.google.mlkit.vision.barcode.common.Barcode
+import com.google.mlkit.vision.common.InputImage
+import timber.log.Timber
+import java.util.concurrent.TimeUnit
+
+@SuppressLint("UnsafeOptInUsageError")
+class BarCodeAnalyser(
+    private val onBarcodeDetected: (barcodes: List<Barcode>) -> Unit,
+) : ImageAnalysis.Analyzer {
+    private var lastAnalyzedTimeStamp = 0L
+
+    override fun analyze(image: ImageProxy) {
+        val currentTimestamp = System.currentTimeMillis()
+        if (currentTimestamp - lastAnalyzedTimeStamp >= TimeUnit.SECONDS.toMillis(1)) {
+            image.image?.let { imageToAnalyze ->
+                val options = BarcodeScannerOptions.Builder()
+                    .setBarcodeFormats(Barcode.FORMAT_ALL_FORMATS)
+                    .build()
+                val barcodeScanner = BarcodeScanning.getClient(options)
+                val imageToProcess = InputImage.fromMediaImage(
+                    imageToAnalyze,
+                    image.imageInfo.rotationDegrees
+                )
+
+                barcodeScanner.process(imageToProcess)
+                    .addOnSuccessListener { barcodes ->
+                        if (barcodes.isNotEmpty()) {
+                            onBarcodeDetected(barcodes)
+                        } else {
+                            Timber.d("analyze: No barcode Scanned")
+                        }
+                    }
+                    .addOnFailureListener { exception ->
+                        Timber.d("BarcodeAnalyser: Something went wrong $exception")
+                    }
+                    .addOnCompleteListener {
+                        image.close()
+                    }
+            }
+            lastAnalyzedTimeStamp = currentTimestamp
+        } else {
+            image.close()
+        }
+    }
+}
