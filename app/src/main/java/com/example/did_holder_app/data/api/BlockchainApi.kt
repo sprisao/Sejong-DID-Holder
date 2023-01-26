7ecc920379fc237d9871730dfc0defd598f64876
sprisao <whdtmek@gmail.com>
2023-01-26 10:15:33 +0900

    리팩터링 : BlockChainHolderApi.kt 클래스명 변경

--- app/src/main/java/com/example/did_holder_app/data/api/BlockchainApi.kt
+++ app/src/main/java/com/example/did_holder_app/data/api/BlockchainApi.kt
@@ -5,7 +5,7 @@ import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.POST
 
-interface BlockChainHolderApi {
+interface BlockchainApi {
     @POST("/v1/holder")
     suspend fun postDidDocument(@Body blockchainHolder: BlockchainHolder): Response<Unit>
 }
