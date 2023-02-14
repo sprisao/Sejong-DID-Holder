94a1806cc82cb3f1d1ae296f5ad548a6ececff06
sprisao <whdtmek@gmail.com>
2023-02-14 17:58:53 +0900

    VC 발급을 위한 통신 확인

--- app/src/main/java/com/example/did_holder_app/ui/VCScreen.kt
+++ app/src/main/java/com/example/did_holder_app/ui/VCScreen.kt
@@ -14,7 +14,9 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.example.did_holder_app.data.api.RetrofitInstance.vcServerApi
-import com.example.did_holder_app.data.model.VC.VC
+import com.example.did_holder_app.data.model.DIDDocument.DidDocument
+import com.example.did_holder_app.data.model.VC.VCRequest
+import com.example.did_holder_app.data.model.VC.VCResponse
 import com.example.did_holder_app.util.Constants
 import com.example.did_holder_app.util.DidDataStore
 import com.squareup.moshi.JsonAdapter
@@ -22,13 +24,15 @@ import com.squareup.moshi.Moshi
 import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.launch
+import okhttp3.internal.userAgent
 import retrofit2.Call
 import retrofit2.Response
+import timber.log.Timber
 
-val jsonAdapter : JsonAdapter<VC> = Moshi.Builder()
+val jsonAdapter: JsonAdapter<VCResponse> = Moshi.Builder()
     .add(KotlinJsonAdapterFactory())
     .build()
-    .adapter(VC::class.java)
+    .adapter(VCResponse::class.java)
 
 @Composable
 fun VCScreen(navController: NavController) {
@@ -37,15 +41,18 @@ fun VCScreen(navController: NavController) {
     /*set vcText state*/
 
     val dataStore = DidDataStore(context)
-    val myVC = dataStore.vcFlow.collectAsState(initial = VC())
+    val myVCResponse = dataStore.vcResponseFlow.collectAsState(initial = VCResponse())
 
     var showVCString by remember {
         mutableStateOf(false)
     }
-    val myVCString = jsonAdapter.toJson(myVC.value)
+    val myVCString = jsonAdapter.toJson(myVCResponse.value)
+    val myDidDocument = dataStore.didDocumentFlow.collectAsState(initial = DidDocument())
 
 //    val myVCString = remember { mutableStateOf(VC(data = null, code = null, msg = "")) }
 
+    /*get userSeqFlow*/
+    val userSeq = dataStore.userseqFlow.collectAsState(0)
 
     Column(
         modifier = Modifier
@@ -54,7 +61,37 @@ fun VCScreen(navController: NavController) {
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.Center
     ) {
-//        if (myVC.value?.data != null) {
+
+        if (userSeq.value != 0 && myVCResponse.value?.data == null) {
+            /*회원가입=true, VC발급=false*/
+
+            Button(onClick = {
+                scope.launch {
+                    getVC(dataStore, scope, userSeq.value!!, myDidDocument.value!!)
+                }
+            }) {
+                Text(text = "VC생성", style = MaterialTheme.typography.labelSmall)
+            }
+        } else if (userSeq.value != 0 && myVCResponse.value?.data != null) {
+            /*회원가입=true, VC발급=true*/
+
+            Button(onClick = {
+
+            }) {
+                Text(text = "VC삭제", style = MaterialTheme.typography.labelSmall)
+            }
+        } else {
+            /*회원가입=false, VC발급=false*/
+
+            Button(onClick = {
+                navController.navigate(Constants.SIGN_UP) {
+                }
+            }) {
+                Text(text = "회원가입", style = MaterialTheme.typography.labelSmall)
+            }
+        }
+    }
+    //        if (myVC.value?.data != null) {
 //            for (i in myVC.value!!.data!!.credentialSubject) {
 //                Column(modifier = Modifier.padding(16.dp), horizontalAlignment = Alignment.Start) {
 //                    Text(text = "이름: ${i.name}", )
@@ -92,28 +129,31 @@ fun VCScreen(navController: NavController) {
 //                Text(text = "사원증 발급 요청(VC)", style = MaterialTheme.typography.labelSmall)
 //            }
 //        }
-        Button(onClick = {navController.navigate(Constants.SIGN_UP){
-        } }) {
-            Text(text = "회원가입", style = MaterialTheme.typography.labelSmall)
-        }
-
-    }
 }
 
-private fun getVC(dataStore: DidDataStore, scope: CoroutineScope) {
-    val call = vcServerApi.getVC("tibob44", "한현수")
-    call.enqueue(object : retrofit2.Callback<VC> {
-        override fun onResponse(call: Call<VC>, response: Response<VC>) {
+fun getVC(dataStore: DidDataStore, scope: CoroutineScope, userSeq: Int, didDocument: DidDocument) {
+    val vcRequest = VCRequest(
+        userseq = userSeq,
+        holderdid = didDocument.id,
+    )
+
+    val call = vcRequest?.let { vcServerApi.getVC(it) }
+    call!!.enqueue(object : retrofit2.Callback<VCResponse> {
+        override fun onResponse(call: Call<VCResponse>, response: Response<VCResponse>) {
             if (response.isSuccessful) {
                 /*set vcText state*/
                 val vcJson = jsonAdapter.toJson(response.body())
                 scope.launch { dataStore.saveVc(vcJson) }
+                /*show modal*/
+                Timber.d(response.body().toString())
+                Timber.d("VC발급 성공")
+
             } else {
                 println("Error")
             }
         }
 
-        override fun onFailure(call: Call<VC>, t: Throwable) {
+        override fun onFailure(call: Call<VCResponse>, t: Throwable) {
             println(t.message)
         }
     })
