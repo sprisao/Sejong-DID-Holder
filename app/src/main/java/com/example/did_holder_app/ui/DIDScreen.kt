de1bb591f5b258326bfbcdb8bbfe5ae51dfe950f
sprisao <whdtmek@gmail.com>
2023-03-06 11:25:56 +0900

    DID 스크린 플립 카드 디자인 추가

--- app/src/main/java/com/example/did_holder_app/ui/DIDScreen.kt
+++ app/src/main/java/com/example/did_holder_app/ui/DIDScreen.kt
@@ -3,18 +3,28 @@ package com.example.did_holder_app.ui
 import android.os.Build
 import android.widget.Toast
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
+import androidx.compose.animation.core.FastOutSlowInEasing
+import androidx.compose.animation.core.animateFloatAsState
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.example.did_holder_app.R
 import com.example.did_holder_app.data.model.DIDDocument.DidDocument
 import com.example.did_holder_app.ui.viewmodel.DIDViewModel
 import kotlinx.coroutines.CoroutineScope
@@ -83,51 +93,224 @@ fun WithDidScreen(
     didDocument: DidDocument
 ) {
     val context = LocalContext.current
+    var cardFace by remember {
+        mutableStateOf(CardFace.Front)
+    }
+
     Column(
         modifier = Modifier
             .fillMaxSize()
             .padding(16.dp),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.Top
     ) {
-        Text(
-            didDocument.id,
-            style = MaterialTheme.typography.bodySmall,
-        )
+        FlipCard(
+            cardFace = cardFace,
+            onClick = { cardFace = cardFace.next },
+            modifier = Modifier
+                .fillMaxWidth(1f)
+                .aspectRatio(0.6f),
+            front = {
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize(),
+                        verticalArrangement = Arrangement.Top,
 
-        Button(
-            onClick = {
-                scope.launch {
-                    viewModel.clearDidDocument()
-                }
-            },
-            modifier = Modifier.padding(16.dp)
-        ) {
-            Text(text = "DID 삭제")
-        }
-        Button(
-            onClick = {
-                scope.launch {
-                    Timber.d(didDocument.toString())
-                }
-            },
-        ) {
-            Text(text = "Did Doc 생성")
-        }
-        Button(onClick = {
-            viewModel.saveDidDocumentToBlockchain(didDocument) {
-                if (it.isSuccessful) {
-                    if (it.body()?.code == 0) {
-                        Toast.makeText(context, "블록체인에 저장되었습니다.", Toast.LENGTH_SHORT).show()
-                    } else {
-                        Toast.makeText(context, "실패 : ${it.body()?.msg}", Toast.LENGTH_SHORT).show()
+                        ) {
+                        Box(
+                            modifier = Modifier
+                                .fillMaxWidth(1f)
+                                .fillMaxHeight(0.7f)
+                                .background(Color.Red),
+                            contentAlignment = Alignment.Center,
+                        ) {
+                            Column(
+                                modifier = Modifier
+                                    .padding(bottom = 10.dp)
+                                    .fillMaxHeight(1f)
+                                    .fillMaxWidth(1f)
+                                ,
+                                verticalArrangement = Arrangement.Bottom,
+                            ) {
+                                Box(
+                                   modifier = Modifier
+                                       .fillMaxWidth(1f)
+                                       .fillMaxHeight(0.7f),
+                                    contentAlignment = Alignment.Center
+
+                                ){
+                                    Image(
+                                        modifier = Modifier
+                                            .fillMaxWidth(0.4f)
+                                            .fillMaxHeight(0.4f),
+                                        painter = painterResource(id = R.drawable.sejong_ci),
+                                        contentDescription = "did holder app logo"
+                                    )
+                                }
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 14.dp),
+                                    text = "SEJONG DID",
+                                    style = TextStyle(
+                                        color = Color.White,
+                                        fontSize = 30.sp,
+                                        fontWeight = FontWeight.Medium
+                                    )
+                                )
+                                Text(
+                                    modifier = Modifier.padding(horizontal = 14.dp),
+                                    text = "자기주권신원인증 시스템",
+                                    style = TextStyle(
+                                        color = Color.White,
+                                        fontSize = 20.sp,
+                                        fontWeight = FontWeight.Normal
+                                    )
+                                )
+                            }
+                        }
+                        Column(
+                            modifier = Modifier
+                                .padding(14.dp)
+                                .fillMaxWidth(1f)
+                                .fillMaxHeight(1f),
+                            verticalArrangement = Arrangement.SpaceBetween,
+                        ) {
+                            Column() {
+                                Text("나의 DID", style = TextStyle(
+                                    color = Color.Black,
+                                    fontSize = 16.sp,
+                                    fontWeight = FontWeight.SemiBold,
+                                    letterSpacing = 0.sp
+                                ))
+                                Text(
+                                    text = didDocument.id,
+                                    style = TextStyle(
+                                        color = Color.Black,
+                                        fontSize = 11.sp,
+                                        fontWeight = FontWeight.SemiBold,
+                                    )
+                                )
+                            }
+                            Button(
+                                modifier = Modifier.fillMaxWidth(1f),
+                                colors = ButtonDefaults.buttonColors(
+                                    contentColor = Color.White
+                                ),
+                                shape = RoundedCornerShape(10.dp),
+                                onClick = {
+                                viewModel.saveDidDocumentToBlockchain(didDocument) {
+                                    if (it.isSuccessful) {
+                                        if (it.body()?.code == 0) {
+                                            Toast.makeText(
+                                                context,
+                                                "블록체인에 저장되었습니다.",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        } else {
+                                            Toast.makeText(
+                                                context,
+                                                "실패 : ${it.body()?.msg}",
+                                                Toast.LENGTH_SHORT
+                                            ).show()
+                                        }
+                                    } else {
+                                        Toast.makeText(
+                                            context,
+                                            "실패 : ${it.body()?.msg}",
+                                            Toast.LENGTH_SHORT
+                                        ).show()
+                                    }
+                                }
+                            }) {
+                                Text(text = "블록체인에 저장")
+                            }
+
+                        }
                     }
-                } else {
-                    Toast.makeText(context, "실패 : ${it.body()?.msg}", Toast.LENGTH_SHORT).show()
+            },
+            back = {
+                Box(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .background(Color.Red),
+                    contentAlignment = Alignment.Center,
+                ) {
+                    Text(
+                        text = "back",
+                        style = MaterialTheme.typography.headlineMedium,
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White),
+                    )
+                    Button(
+                        onClick = {
+                            scope.launch {
+                                viewModel.clearDidDocument()
+                            }
+                        },
+                    ) {
+                        Text(text = "DID 삭제")
+                    }
                 }
+            },
+        )
+    }
+}
+
+enum class CardFace(val angle: Float) {
+    Front(180f) {
+        override val next: CardFace
+            get() = Back
+    },
+    Back(0f) {
+        override val next: CardFace
+            get() = Front
+    };
+
+    abstract val next: CardFace
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun FlipCard(
+    cardFace: CardFace,
+    onClick: (CardFace) -> Unit,
+    modifier: Modifier = Modifier,
+    front: @Composable () -> Unit = {},
+    back: @Composable () -> Unit = {},
+) {
+    val rotation = animateFloatAsState(
+        targetValue = cardFace.angle,
+        animationSpec = tween(durationMillis = 500, easing = FastOutSlowInEasing)
+    ).value
+    Card(
+        elevation = CardDefaults.cardElevation(8.dp),
+        onClick = { onClick(cardFace) },
+        modifier = modifier.graphicsLayer {
+            rotationY = rotation
+            cameraDistance = 12f * density
+        },
+        colors = CardDefaults.cardColors(
+            containerColor = Color.White,
+        ),
+    ) {
+        if (rotation <= 90f) {
+            Box(
+                Modifier.fillMaxSize()
+            ) {
+                back()
             }
-        }) {
-            Text(text = "블록체인에 저장")
+        } else {
+            Box(
+                Modifier
+                    .fillMaxSize()
+                    .graphicsLayer {
+                        rotationY = 180f
+                    }
+            ) {
+                front()
+            }
         }
     }
 }
